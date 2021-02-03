a6254999d8a5ee2f0be2119904c48b8edcaf98a1
Pwootage <pwootage@gmail.com>
2021-02-02 23:31:30 -0700

    Basic command based group finder. No fancy features.

--- core.lua
+++ core.lua
@@ -11,7 +11,7 @@ GuildGroupFinder = LibStub('AceAddon-3.0'):NewAddon(
 local AceGUI = LibStub('AceGUI-3.0')
 addon.GuildGroupFinder = GuildGroupFinder
 
-GuildGroupFinder.COMM_PREFIX = "ggf"
+GuildGroupFinder.COMM_PREFIX = 'ggf'
 GuildGroupFinder.DEBUG = true
 
 -- init, enable, disable
@@ -19,24 +19,147 @@ function GuildGroupFinder:OnInitialize()
   self:InitOptions()
   -- Set up empty player status
   self.player_status = {}
+
+  self:RegisterEvent('CHAT_MSG_GUILD', function(evt, msg, author) self:HandleMessage(msg, author) end)
+  self:RegisterEvent('CHAT_MSG_WHISPER', function(evt, msg, author) self:HandleMessage(msg, author) end)
+  self:RegisterEvent('CHAT_MSG_PARTY', function(evt, msg, author) self:HandleMessage(msg, author) end)
+  self:RegisterEvent('CHAT_MSG_PARTY_LEADER', function(evt, msg, author) self:HandleMessage(msg, author) end)
+
+
+  self:RegisterChatCommand('grclear', 'RollClear')
+  self:RegisterChatCommand('grlist', 'RollList')
+  self:RegisterChatCommand('grstart', 'RollStart')
+  self:RegisterChatCommand('grend', 'RollEnd')
+  self:RegisterChatCommand('groll', 'RollEnd')
+  self:RegisterChatCommand('gradd', 'RollAdd')
+  self:RegisterChatCommand('grdel', 'RollAdd')
+  self:RegisterChatCommand('grremove', 'RollRemove')
 end
 
 function GuildGroupFinder:OnEnable()
-  -- Listen for our comms
-  self:RegisterComm(self.COMM_PREFIX)
-
-  -- Hack our frame into the PVEFrame
-  self:Hook('PVEFrame_ShowFrame', function() GuildGroupFinder_FinderFrame:Hide() end, true)
-  PVEFrame.tab4 = GuildGroupFinderTab
-  PanelTemplates_SetNumTabs(PVEFrame, 4)
-  PVEFrame.maxTabWidth = (PVEFrame:GetWidth() - 19) / 4;
-
-  -- Sync data
-  self:ForceFullSync()
+  self.players = {}
+  self.currentRoll = {}
+  self.rolling = false
 end
 
 function GuildGroupFinder:OnDisable()
   -- No reason to undo anything right now
+end
+
+function GuildGroupFinder:HandleMessage(msg, author)
+  if not self.rolling then
+    return
+  end
+  -- self:Print('Got message', msg, author)
+  if msg:find('^1') ~= nil then
+    self:RegisterPlayer(author)
+    ChatThrottleLib:SendChatMessage(
+      'NORMAL', 'ggf',
+      'Added you to the roll',
+      'WHISPER', nil, player
+    )
+  elseif msg:find('^0') ~= nil then
+    self:UnregisterPlayer(author)
+      ChatThrottleLib:SendChatMessage(
+      'NORMAL', 'ggf',
+      'Removed you from the roll.',
+      'WHISPER', nil, player
+    )
+  end
+end
+
+function GuildGroupFinder:RollClear()
+  self.players = {}
+  self:Print('Clearing the list of players')
+end
+
+
+function GuildGroupFinder:RollStart()
+  self.rolling = true
+  local message = 'Starting a guild roll. Post a message starting with 1 to join.'
+  self:RollList()
+  local name, realm = UnitName('player')
+  self:RegisterPlayer(name)
+  ChatThrottleLib:SendChatMessage(
+    'NORMAL', 'ggf',
+    'Starting a guild roll. Post a message starting with 1 to join, or 0 to leave if you change your mind.',
+    'GUILD', nil, nil
+  )
+end
+
+function GuildGroupFinder:RollList()
+  local message = ''
+  local count = 0
+  for player, v in pairs(self.players) do
+    message = message .. player .. ', '
+    count = count + 1
+    if (count >= 10) then
+      self:Print(message)
+      message = ''
+    end
+  end
+  if (message ~= '') then
+    self:Print(message)
+  end
+  self:Print('Currently there are ' .. count .. ' players in the roll')
+
+end
+
+function GuildGroupFinder:RollEnd()
+  self.rolling = false
+  self:RollList()
+  -- make a list table
+  local rollList = {}
+  local i = 1
+  for player, v in pairs(self.players) do
+    rollList[i] = player
+    i = i + 1
+  end
+  -- shuffle the list
+  for i = 1, #rollList do
+    local j = math.random(i)
+		rollList[i], rollList[j] = rollList[j], rollList[i]
+  end
+  -- Print the first 5
+  local message = 'Group:'
+  for i = 1, math.min(5, #rollList) do
+    message = message .. ' ' .. rollList[i]
+  end
+  ChatThrottleLib:SendChatMessage(
+    'NORMAL', 'ggf',
+    message,
+    'GUILD', nil, nil
+  )
+end
+
+function GuildGroupFinder:RegisterPlayer(player)
+  local name = player:match('^([^-]+)\\-')
+  if name == nil then
+    name = player
+  end
+  self.players[name] = true
+  self:Print('Added ' .. name)
+end
+
+function GuildGroupFinder:UnregisterPlayer(player)
+  local name = player:match('^([^-]+)\\-')
+  if name == nil then
+    name = player
+  end
+  self.players[name] = nil
+  self:Print('Removed ' .. name)
+end
+
+function GuildGroupFinder:RollAdd(input)
+  for player in input:gmatch('(%S+)%s*') do
+    self:RegisterPlayer(player)
+  end
+end
+
+function GuildGroupFinder:RollRemove(input)
+  for player in input:gmatch('(%S+)%s*') do
+    self:UnregisterPlayer(player)
+  end
 end
 
 -- Utility methods
--- frames/group.lua
+++ frames/group.lua
@@ -3,108 +3,67 @@ local addonName, addon = ...
 local AceGUI = LibStub('AceGUI-3.0')
 
 function PVEFrameTab4_OnClick(self)
-  -- blindly hide the old tabs (professional, I know)
-  local tab1 = _G['GroupFinderFrame']
-  local tab2 = _G['PVPUIFrame']
-  local tab3 = _G['ChallengesFrame']
-  if tab1 then tab1:Hide() end
-  if tab2 then tab2:Hide() end
-  if tab3 then tab3:Hide() end
-
   PlaySound(SOUNDKIT.IG_CHARACTER_INFO_TAB);
-
-  -- Code to show the tab copied from the original code
-  ShowUIPanel(PVEFrame);
-  PVEFrame.activeTabIndex = tabIndex
-  PanelTemplates_SetTab(PVEFrame, 4)
-  PVEFrame:SetWidth(PVE_FRAME_BASE_WIDTH)
-  UpdateUIPanelPositions(PVEFrame)
-  GuildGroupFinder_FinderFrame:Show()
+  GuildGroupFinder:ShowRollUI()
 end
 
--- Guild finder frame itself
-GuildGroupFinder_FinderFrameMixin = {
-  playerFrames = {}
-}
+function GuildGroupFinder:ShowRollUI()
+  local f = AceGUI:Create('Frame')
+  f:SetCallback('OnClose',function(widget) AceGUI:Release(widget) end)
+  f:SetTitle('Group Roll')
+  f:SetStatusText('')
+  f:SetLayout('List')
 
-function GuildGroupFinder_FinderFrameMixin:OnLoad()
-  local frame = AceGUI:Create('SimpleGroup')
+  local btn = AceGUI:Create('Button')
+  btn:SetWidth(250)
+  btn:SetText('Start registration (/grstart)')
+  btn:SetCallback('OnClick', function() GuildGroupFinder:RollStart() end)
+  f:AddChild(btn)
 
-end
+  local btn = AceGUI:Create('Button')
+  btn:SetWidth(250)
+  btn:SetText('Roll (/groll)')
+  btn:SetCallback('OnClick', function() GuildGroupFinder:RollEnd() end)
+  f:AddChild(btn)
 
-function GuildGroupFinder_FinderFrameMixin:OnShow()
-  PVEFrame:SetPortraitToAsset('Interface\\Icons\\achievement_bg_wineos_underxminutes')
-  PVEFrame.TitleText:SetText('Guild Finder')
-  PVEFrame_HideLeftInset()
-  -- GuildGroupFinder_FinderFrame_Update(self);
 
-  self:Update()
-end
+  local btn = AceGUI:Create('Button')
+  btn:SetWidth(250)
+  btn:SetText('Print current list (/grlist)')
+  btn:SetCallback('OnClick', function() GuildGroupFinder:RollList() end)
+  f:AddChild(btn)
 
-function GuildGroupFinder_FinderFrameMixin:OnHide()
-  PVEFrame_ShowLeftInset()
-end
+  local btn = AceGUI:Create('Button')
+  btn:SetWidth(250)
+  btn:SetText('Clear List (/grclear)')
+  btn:SetCallback('OnClick', function() GuildGroupFinder:RollClear() end)
+  f:AddChild(btn)
 
-function GuildGroupFinder_FinderFrame_OnValueChanged(self, value)
-  GuildGroupFinder_FinderFrame_ScrollToValue(self, value)
-end
+  local addGroup = AceGUI:Create('InlineGroup')
+  addGroup:SetLayout('Flow')
+  f:AddChild(addGroup)
 
-function GuildGroupFinder_FinderFrame_ScrollToValue(self, value)
-  local height = GuildGroupFinder_FinderFrame_ScrollChild:GetHeight() - GuildGroupFinder_FinderFrame_ScrollFrame:GetHeight()
-  local offset = height * (value / 1000)
-  GuildGroupFinder:Debug('Height; ', height, offset)
-  GuildGroupFinder_FinderFrame_ScrollFrame:SetVerticalScroll(offset)
-end
+  local addText = AceGUI:Create('EditBox')
+  addText:DisableButton(true)
+  addGroup:AddChild(addText)
 
-function GuildGroupFinder_FinderFrame_OnScroll(self, value)
-  local scroll = GuildGroupFinder_FinderFrame_ScrollFrame:GetVerticalScroll()
-  local value = scroll + value * 16
-  value = math.min(math.max(value, 0), 1000)
+  local btn = AceGUI:Create('Button')
+  btn:SetWidth(250)
+  btn:SetText('Add to list (/gradd <a> <b> <c>)')
+  btn:SetCallback('OnClick', function() GuildGroupFinder:RollAdd(addText:GetText()) end)
+  addGroup:AddChild(btn)
 
-  GuildGroupFinder_FinderFrame_Slider:SetValue(value)
-end
+  local removeGroup = AceGUI:Create('InlineGroup')
+  removeGroup:SetLayout('Flow')
+  f:AddChild(removeGroup)
 
-function GuildGroupFinder_FinderFrameMixin:Update()
-  -- Create the most recent list
-  -- hide existing list items
-  for  index, frame in ipairs(self.playerFrames) do
-    frame:Hide()
-  end
+  local removeText = AceGUI:Create('EditBox')
+  removeText:DisableButton(true)
+  removeGroup:AddChild(removeText)
 
-  -- Build the list of players
-  local players = {}
-  for i = 1, 10, 1 do
-    for player, status in pairs(GuildGroupFinder.player_status) do
-        table.insert(players, {
-          name = player,
-          status = status
-        })
-    end
-  end
-  table.sort(players, function(a,b) return a.name < b.name end)
-
-  -- Set up the UI
-  local last = nil
-  for index, player in ipairs(players) do
-    local frame = self.playerFrames[index]
-    if frame == nil then
-      GuildGroupFinder:Debug('Creating frame for player', index, player.name, player.status)
-      frame = CreateFrame('Frame', 'GuildGroupFinder_PlayerFrameTemplate'..index, GuildGroupFinder_FinderFrame_ScrollChild, 'GuildGroupFinder_PlayerFrameTemplate')
-      self.playerFrames[index] = frame
-    else
-      GuildGroupFinder:Debug('Reusing frame for player', index, player.name, player.status)
-    end
-
-    frame.player = player.name
-    frame.status = player.status
-    frame:ClearAllPoints()
-    frame:SetSize(50, 50)
-    if last == nil then
-      frame:SetPoint('TOPLEFT', GuildGroupFinder_FinderFrame_ScrollChild, 'TOPLEFT', 50, -50)
-    else
-      frame:SetPoint('TOPLEFT', last, 'BOTTOMLEFT', 0, -4)
-    end
-    frame:Show()
-    last = frame
-  end
+  local btn = AceGUI:Create('Button')
+  btn:SetWidth(250)
+  btn:SetText('Remove list (/grremove <a> <b> <c>)')
+  btn:SetCallback('OnClick', function() GuildGroupFinder:RollRemove(removeText:GetText()) end)
+  removeGroup:AddChild(btn)
 end
